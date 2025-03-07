85e8aec5bdfba60f605c4780878b14d4fa6af3e0
Nightfury456 <ak7217@srmist.edu.in>
2025-03-07 22:09:11 +0530

    using import

---
+++ SimpleStorage.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract SimpleStorage {
+    // if no value is given then it defaults to 0
+    uint256 myFavNum;
+
+    // uint256[] listFavNum;
+    struct Person {
+        uint256 favNum;
+        string name;
+    }
+
+    // dynamic array
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavNum;
+
+    function store(uint256 newFavNum) public {
+        myFavNum = newFavNum;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return myFavNum;
+    }
+
+    // calldata, memory,storage
+    function addPerson(string memory _name, uint256 _favNum) public {
+        listOfPeople.push(Person(_favNum, _name));
+        nameToFavNum[_name] = _favNum;
+    }
+
+}
