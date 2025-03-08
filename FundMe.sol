480a7af3448b55de0a8d11bbc0a785992d0cfb44
Nightfury456 <ak7217@srmist.edu.in>
2025-03-08 22:10:34 +0530

    fund

--- FundMe.sol
+++ FundMe.sol
@@ -3,11 +3,14 @@
 pragma solidity 0.8.19;
 
 contract FundMe {
+
+    uint256 public minimumUsd = 5;
+
     function fund() public payable {
         // allow users to send $
-        // have a minimum $ sent
+        // have a minimum 5$ sent
         // How do we send ETH to this contract?
-        require(msg.value > 1e18, "Didn't send the correct amount of ETH");
+        require(msg.value >= minimumUsd, "Didn't send the correct amount of ETH");
     }
 
     // function withdraw() public {}
