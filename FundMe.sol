e1553804a10e9077414149a525b3f72205d31e4f
Nightfury456 <ak7217@srmist.edu.in>
2025-03-10 01:17:13 +0530

    getting price data

--- FundMe.sol
+++ FundMe.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.8.19;
 
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
 
     uint256 public minimumUsd = 5;
@@ -14,4 +16,10 @@ contract FundMe {
     }
 
     // function withdraw() public {}
+
+    function getPrice() public {
+        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+    }
+    function getCOnversionRate() public {}
 }
