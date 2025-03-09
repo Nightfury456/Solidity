183096f32526190398f5e36fca04fda9d356dd0e
Nightfury456 <ak7217@srmist.edu.in>
2025-03-10 02:12:06 +0530

    keep tract of funders and their amounts

--- FundMe.sol
+++ FundMe.sol
@@ -8,11 +8,16 @@ contract FundMe {
 
     uint256 public minimumUsd = 5e18;
 
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
     function fund() public payable {
         // allow users to send $
         // have a minimum 5$ sent
         // How do we send ETH to this contract?
         require(getCOnversionRate(msg.value) >= minimumUsd, "Didn't send the correct amount of ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     // function withdraw() public {}
