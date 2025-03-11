6a3fb55e1e1e6f90826a028ba90e05bc1314c41d
Nightfury456 <ak7217@srmist.edu.in>
2025-03-11 11:08:29 +0530

    call, constructor

--- FundMe.sol
+++ FundMe.sol
@@ -12,6 +12,12 @@ contract FundMe {
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
 
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send the correct amount of ETH");
@@ -20,10 +26,20 @@ contract FundMe {
     }
 
     function withdraw() public {
+        require(msg.sender == owner, "Must be owner");
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
+
+        // reset the array
+        funders = new address[](0);
+
+        // actually withdraw the funds
+        // call - 
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+        
     }
 
 }
